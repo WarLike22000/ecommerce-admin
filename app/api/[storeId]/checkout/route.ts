88be784c2c50f5b96629dd859cee55b7a874b352
Alireza Kamali <kamalialireza788@gmail.com>
2023-10-22 14:18:03 +0330

    FIANL

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -2,15 +2,15 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/app/libs/prismadb";
 
-// const corsHeaders = {
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-//     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-// };
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
-// export async  function OPTIONS() {
-//     return NextResponse.json({}, { headers: corsHeaders })
-// };
+export async  function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders })
+};
 
 export async function POST(
     req: Request,
